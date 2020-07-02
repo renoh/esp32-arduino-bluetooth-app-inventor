6d31aaea7e8c81cb1b500fc540d597405908536a
Henrique Reno Sawada <renohh@gmail.com>
2020-07-02 00:18:28 -0300

    piscando 4 LEDs sequencialmente

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define LED_R 15
+#define LED_G 4
+#define LED_Y 5
+#define LED_B 2
+
+void setup()
+{
+    pinMode(LED_R, OUTPUT);
+    pinMode(LED_G, OUTPUT);
+    pinMode(LED_Y, OUTPUT);
+    pinMode(LED_B, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    digitalWrite(LED_R, HIGH);
+    digitalWrite(LED_B, LOW);
+    delay(500);
+    digitalWrite(LED_Y, HIGH);
+    digitalWrite(LED_R, LOW);
+    delay(500);
+    digitalWrite(LED_G, HIGH);
+    digitalWrite(LED_Y, LOW);
+    delay(500);
+    digitalWrite(LED_B, HIGH);
+    digitalWrite(LED_G, LOW);
+    delay(500);
 }
